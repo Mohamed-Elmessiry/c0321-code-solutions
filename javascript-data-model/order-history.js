372f064930cdc0f37bb9da073780c18a13dda486
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-03-16 15:01:00 -0400

    completed js data model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,32 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4th, 2020',
+    total: '$34',
+    products: [{ name: 'JavaScript for impatient programmers' }],
+    author: 'Rauschmayer, Dr.Axel',
+    shipTo: 'JS Masher',
+    images: [{ image1: 'url' }]
+  },
+  {
+    orderPlaced: 'July 19th, 2020',
+    total: '$44.53',
+    products: [{ name: 'The Timeless Way of Building' }],
+    author: 'Alexander Christopher',
+    shipTo: 'JS Masher',
+    images: [{ image1: 'url' }]
+  },
+  {
+    orderPlaced: 'July 4th, 2020',
+    total: '$17.22',
+    products: [{ name: 'Gamecube Contoller Adapter, Super Smash Bros Switch Gamecube Adapter for  WII U, PC Support Turbo and Vibration Features. No Driver and No Lag- Gamecube Adapter' }],
+    shipTo: 'JS Masher',
+    images: [{ image1: 'url' }]
+  },
+  {
+    orderPlaced: 'July 3rd, 2020',
+    total: '$138.93',
+    products: [{ name: 'GameCube Controller- Super Smash Bros Edition (Nintendo Switch)' }, { name: 'The Art of Sql' }],
+    shipTo: 'JS Masher',
+    images: [{ image1: 'url' }, { image2: 'url2' }]
+  }
+];
