eae9abff2e4bdae09606ec8ad54fa71bfcc2308f
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-03-16 15:54:35 -0400

    amended again

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -2,30 +2,28 @@ var orderHistory = [
   {
     orderPlaced: 'August 4th, 2020',
     total: '$34',
-    products: [{ name: 'JavaScript for impatient programmers', image: 'url' }],
-    author: 'Rauschmayer, Dr.Axel',
+    products: [{ name: 'JavaScript for impatient programmers', image: 'url', author: 'Rauschmayer, Dr.Axel', cost: '$31.55' }],
     shipTo: 'JS Masher'
 
   },
   {
     orderPlaced: 'July 19th, 2020',
     total: '$44.53',
-    products: [{ name: 'The Timeless Way of Building', image: 'url' }],
-    author: 'Alexander Christopher',
+    products: [{ name: 'The Timeless Way of Building', image: 'url', author: 'Alexander Christopher', cost: '$41.33' }],
     shipTo: 'JS Masher'
 
   },
   {
     orderPlaced: 'July 4th, 2020',
     total: '$17.22',
-    products: [{ name: 'Gamecube Contoller Adapter, Super Smash Bros Switch Gamecube Adapter for  WII U, PC Support Turbo and Vibration Features. No Driver and No Lag- Gamecube Adapter', image: 'url' }],
+    products: [{ name: 'Gamecube Contoller Adapter, Super Smash Bros Switch Gamecube Adapter for  WII U, PC Support Turbo and Vibration Features. No Driver and No Lag- Gamecube Adapter', image: 'url', price: '$15.98' }],
     shipTo: 'JS Masher'
 
   },
   {
     orderPlaced: 'July 3rd, 2020',
     total: '$138.93',
-    products: [{ name: 'GameCube Controller- Super Smash Bros Edition (Nintendo Switch)', image: 'url' }, { name: 'The Art of Sql', image: 'url' }],
+    products: [{ name: 'GameCube Controller- Super Smash Bros Edition (Nintendo Switch)', image: 'url', price: '$94.95' }, { name: 'The Art of Sql', author: 'Faroult, Stephane', image: 'url', price: '$33.99' }],
     shipTo: 'JS Masher'
 
   }
