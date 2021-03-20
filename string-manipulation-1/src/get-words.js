6b02844aa1baad01635b25409a1b2ebf727ee7f1
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-03-19 20:00:15 -0400

    Deleted loop

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,11 +1,11 @@
 /* exported getWords */
 function getWords(string) {
-  var empty = [];
+
   var newString = string.split(' ');
-  for (var i = 0; i < newString.length; i++) {
-    if (string.length === 0) {
-      return empty;
-    }
+
+  if (string.length === 0) {
+    return [];
   }
+
   return newString;
 }
