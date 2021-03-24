6eb16ad398e9871cf48ec136f69af0f870c45fae
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-03-23 20:16:03 -0400

    Completed JS Modal

---
+++ javascript-modal/main.js
@@ -0,0 +1,17 @@
+
+var mHolder = document.querySelector('.modal-holder');
+var holdingContainer = document.querySelector('.container');
+var modalDiv = document.querySelector('.modal');
+var modalButton = document.querySelector('.modal-button');
+modalButton.addEventListener('click', function () {
+  modalDiv.className = 'modal-active';
+  holdingContainer.className = 'container-pressed';
+  mHolder.className = 'modal-holder-active';
+});
+
+var noButton = document.querySelector('.no-button');
+noButton.addEventListener('click', function () {
+  modalDiv.className = 'modal';
+  holdingContainer.className = 'container';
+  mHolder.className = 'modal';
+});
