27057c7e30d14da3ad322a3d79d0f5699f97e40d
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-03-25 15:59:32 -0400

    completed dom event delegation

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,10 @@
+var taskList = document.querySelector('.task-list');
+taskList.addEventListener('click', function () {
+  console.log(event.target);
+  console.log(event.target.tagName);
+  if (event.target.tagName === 'BUTTON') {
+    var close = event.target.closest('.task-list-item');
+    console.log(close);
+    close.remove();
+  }
+});
