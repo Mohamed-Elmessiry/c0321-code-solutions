bfad79f54f161b5f051b5b2a4baf55405a8bba9e
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-03-25 20:44:00 -0400

    javascript view swapping

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,24 @@
+var tabContainer = document.querySelector('.tab-container');
+var tabElements = document.querySelectorAll('.tab');
+var views = document.querySelectorAll('.view');
+tabContainer.addEventListener('click', swap);
+function swap(event) {
+  if (event.target.matches('.tab')) {
+    for (var i = 0; i < tabElements.length; i++) {
+      if (tabElements[i] === event.target) {
+        tabElements[i].className = 'tab active';
+      } else {
+        tabElements[i].className = 'tab';
+      }
+
+    }
+  }
+  var attr = event.target.getAttribute('data-view');
+  for (var k = 0; k < views.length; k++) {
+    if (views[k].getAttribute('data-view') !== attr) {
+      views[k].className = 'view hidden';
+    } else {
+      views[k].className = 'view';
+    }
+  }
+}
