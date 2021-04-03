b4599e6b1bfcf282005c221d74e55d0eff869578
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-04-03 18:20:27 -0400

    completed string manipulation 2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,8 +1,13 @@
 /* exported swapChars */
-// function swapChars(firstIndex, secondIndex, string) {
-//   var newString = '';
-//   var str1
-//   for (var i = 0; i < string.length; i++) {
-//   }
-//   return newString;
-// }
+function swapChars(firstIndex, secondIndex, string) {
+  var newString = '';
+  var stringDivided = string.split('');
+  var fIndex = stringDivided[firstIndex];
+  var sIndex = stringDivided[secondIndex];
+  stringDivided[firstIndex] = sIndex;
+  stringDivided[secondIndex] = fIndex;
+  for (var i = 0; i < stringDivided.length; i++) {
+    newString += stringDivided[i];
+  }
+  return newString;
+}
