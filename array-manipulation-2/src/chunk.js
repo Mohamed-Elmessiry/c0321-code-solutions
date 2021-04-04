c533c1e7149f76a20885ef939cf3ec477067b79b
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-04-04 13:09:27 -0400

    wip

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,15 @@
 /* exported chunk */
+function chunk(array, size) {
+  var newArray = [];
+  var subArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === newArray[i]) {
+      return newArray;
+    } else {
+      subArray.push(array[i]);
+
+    }
+    newArray.push(subArray);
+  }
+  return newArray;
+}
