7cdfe5e5929908564fcd7077fd8a589ec4932239
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-04-07 18:42:18 -0400

    changes the if statement

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -3,13 +3,7 @@ var counter = 4;
 
 function countdown() {
   heading.textContent = counter;
-  if (counter === 4) {
-    counter--;
-  } else if (counter === 3) {
-    counter--;
-  } else if (counter === 2) {
-    counter--;
-  } else if (counter === 1) {
+  if (counter > 0) {
     counter--;
   } else {
     heading.textContent = '~Earth Beeeelooowww Us~';
