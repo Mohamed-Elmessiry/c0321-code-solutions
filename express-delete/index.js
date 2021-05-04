5f18f9f3be3042108d1707fcfe3841259be0cff5
Mohamed Elmessiry <mohamedelmessiry@hotmail.com>
2021-05-04 19:30:13 +0000

    completed express delete exercise

---
+++ express-delete/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+
+};
+
+app.get('/api/grades', function (req, res) {
+  let key;
+  const array = [];
+  for (key in grades) {
+    array.push(grades[key]);
+
+  }
+  res.json(array);
+
+});
+
+app.delete('/api/grades/:id', function (req, res) {
+  const id = req.params.id;
+  delete grades[id];
+  res.sendStatus(204);
+
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
