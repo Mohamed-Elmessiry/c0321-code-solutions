948f4f8d9e693752aff9d323da32bbbf48079ee6
Mohamed Elmessiry <mohamedelmessiry@hotmail.com>
2021-05-14 22:37:42 +0000

    completed react events and state

--- react-events-and-state/src/index.jsx
+++ react-events-and-state/src/index.jsx
@@ -5,22 +5,24 @@ class CustomButton extends React.Component {
 
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = { isClicked: false };
 
+    this.state = {
+      isClicked: false
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    this.setState = ({ isClicked: true });
+    this.setState({ isClicked: true });
   }
 
   render() {
     const isClicked = this.state.isClicked;
     if (isClicked) {
-      return <button>{'Thanks !'}</button>;
+      return <button>Thanks !</button>;
 
     } else {
-      return <button onClick={this.handleClick}>{'Click Me!'}</button>;
+      return <button onClick={this.handleClick}>Click Me!</button>;
     }
 
   }
