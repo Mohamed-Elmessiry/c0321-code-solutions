d7ebca20e0b6dd3b15f353b10c559a99cf4bffbe
Mohamed-Elmessiry <mohamedelmessiry@hotmail.com>
2021-06-28 17:07:02 -0400

    wip

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,7 +1,14 @@
 /* exported reverseWords */
-// function reverseWords(string) {
-//   var stringArray = string.split(' ');
-//   for (var i = stringArray.length; i > 0 ; i--){
-
-//   }
-// }
+function reverseWords(string) {
+  var reversedWord = '';
+  var wordArray = string.split(' ');
+  for (var i = 0; i < wordArray.length; i++) {
+    var word = wordArray[i];
+    for (var j = word.length - 1; j >= 0; j--) {
+      reversedWord += word[j];
+    }
+    reversedWord += ' ';
+  }
+  reversedWord = reversedWord.slice(0, reversedWord.length - 1);
+  return reversedWord;
+}
